cf48b06433877743a5a401b3f90b3efc537dbb36
TommyNguyen <timsnguyen@hotmail.com>
2023-03-21 15:49:48 -0700

    Backend folder setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const knex = require("knex")(require("./knexfile"));
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
+
+
+app.use(express.json());
+app.use(cors());
+
+const warehouseRoutes = require('./routes/warehouseRoute');
+const inventoryRoutes = require('./routes/inventoryRoute');
+
+app.use('/warehouses', warehouseRoutes);
+app.use('/inventory', inventoryRoutes);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`);
+  });  
