8277660d47fee0831855fa3ce0b3e49453eb9762
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-22 13:51:55 -0700

    add updateItem to inventoryController. Add route to inventoryRoute.js for inventoryController.updateItem

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,15 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventory: ${err}`)
     );
 };
+
+exports.updateItem = (_req, res) => {
+  knex('inventory')
+    .update(req.body)
+    .where({ id: req.params.id })
+    .then(() => {
+      res.status(200).send(`Inventory with id: ${req.params.id} has been updated`);
+    })
+    .catch(() => 
+    res.status(400).send(`Error updating Inventory Item ${req.params.id} ${err}`)
+    );
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,4 +3,6 @@ const inventoryController = require('../controllers/inventoryController');
 
 router.route('/').get(inventoryController.index);
 
+router.route('/inventories/:id').put(inventoryController.updateItem);
+
 module.exports = router;
