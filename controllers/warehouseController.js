a2ad3f54966fbe960a1d3a0276a7e44e491a67e0
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-22 20:04:42 -0700

    add warehouseRoute for editController and start exports.editWarehouse request function in warehouseController.js

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -9,3 +9,37 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Warehouses: ${err}`)
     );
 };
+
+//edit Warehouse
+exports.editWarehouse = (req, res) => {
+  //validation that all things exist in request
+  if (
+		!req.body.warehouse_name ||
+		!req.body.address ||
+		!req.body.city ||
+		!req.body.country ||
+		!req.body.contact_name ||
+		!req.body.contact_position ||
+		!req.body.contact_phone ||
+		!req.body.contact_email
+	) {
+		return res
+			.status(400)
+			.send(
+				"Please make sure to provide all warehouse information in your request, edit failed"
+			);
+	}
+
+  //require valid email address
+  if (!req.body.contact_email.includes("@" || !req.body.contact_email.includes("."))) {
+    return res.status(400).send("Please include a valid email, edit failed")
+  }
+
+  //require valid phone number length
+  if (parseInt(req.body.contact_phone).length < 10) {
+    return res.status(400).send("Please include a valid phone number, edit failed")
+  }
+
+  //////////////////////////////////////left off here Wednesday evening...//////////
+
+}
