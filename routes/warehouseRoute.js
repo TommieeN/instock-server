c9892d7161c37ed8dae00d3072a8a94fe46b9035
TommyNguyen <timsnguyen@hotmail.com>
2023-03-24 10:23:29 -0700

    finish inserting new warehouse to database

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -3,7 +3,8 @@ const warehouseController = require('../controllers/warehouseController');
 
 router
 .route('/')
-.get(warehouseController.index);
+.get(warehouseController.index)
+.post(warehouseController.addNewWarehouse);
 
 router
 .route('/:id')
